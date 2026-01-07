24248f171e9bfa99265dc99408f00b9d3878e047
emmakowu3579-ui <emmakowu3579@gmail.com>
2026-01-07 10:44:59 +0000

    Update baseline_gcn.py

--- starter_code/baseline_gcn.py
+++ starter_code/baseline_gcn.py
@@ -1,1 +1,49 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+from torch_geometric.nn import GCNConv
+from sklearn.metrics import f1_score
+
+# Load dataset
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+data = dataset[0]
+
+# Inductive split
+train_mask = data.train_mask
+test_mask = data.test_mask
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super().__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        return x
+
+model = GCN(
+    in_channels=dataset.num_features,
+    hidden_channels=16,
+    out_channels=dataset.num_classes
+)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# Training
+model.train()
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data.x, data.edge_index)
+    loss = F.cross_entropy(out[train_mask], data.y[train_mask])
+    loss.backward()
+    optimizer.step()
+
+# Evaluation
+model.eval()
+pred = out[test_mask].argmax(dim=1)
+f1 = f1_score(data.y[test_mask].cpu(), pred.cpu(), average='macro')
+print("Macro F1:", f1)
 
